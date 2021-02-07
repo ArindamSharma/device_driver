65ef741f690d2ea721e8dfe0de9fa2a9de97c2bf
ArindamSharma <arindamsharma1998@gmail.com>
2021-02-07 22:10:05 +0530

    lab1 q3 finish

--- Lab1/q3.sh
+++ Lab1/q3.sh
@@ -1,4 +1,17 @@
 read -p "Enter the URL :- " url
-echo $url
-# for i in {0, 10}
-time wget $url
+echo "Webpage :- "$url
+run=9
+time_to_sleep=0
+for i in $(seq 0 $run);
+do
+    wget -o test.txt -E $url
+    array[$i]=$( sed -n 's/\/s).*/\/s/p' test.txt | sed -n 's/.*(//p' )
+    echo "Downloaded" $(($i+1))"st time with speed :- "${array[$i]}
+    sleep $time_to_sleep
+done 
+rm *.html # to delete if not required
+echo "Analysing ..."
+for i in $(seq 0 $run);
+do
+    echo -E ${array[$i]}
+done
